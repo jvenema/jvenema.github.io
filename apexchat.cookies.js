8354309e111689ef9d31bbb4fa796fd17c355afb
Jerod Venema <jerod.venema@frozenmountain.com>
2019-09-16 22:02:36 -0700

    bug fix

--- apexchat.cookies.js
+++ apexchat.cookies.js
@@ -30,93 +30,88 @@ window.addEventListener("message", function(event){
 }, false);
 
 ApexChat.Cookies = {
-        set: function (name, value, days) {
-        	if(ApexChat.framed){
-          	    window.postMessage({
-            	    method: "ApexChat.Cookies.set",
-            	    arguments: Array.prototype.slice.call(arguments)
-                });
-          	    return
+    set: function (name, value, days) {
+        if(ApexChat.framed){
+            window.postMessage({
+                method: "ApexChat.Cookies.set",
+                arguments: Array.prototype.slice.call(arguments)
+            });
+            return
+        }
+        var expires;
+        if (name) {
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toGMTString();
             }
-            var expires;
-            if (name) {
-                if (days) {
-                    var date = new Date();
-                    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                    expires = "; expires=" + date.toGMTString();
-                }
-                else {
-                    expires = "";
-                }
-                document.cookie = name + "=" + escape(value) + expires + "; path=/";
+            else {
+                expires = "";
             }
-        },
-        setShort: function (name, value, seconds) {
-            var expires;
-            if (name) {
-                if (seconds) {
-                    var date = new Date();
-                    date.setTime(date.getTime() + (seconds * 1000));
-                    expires = "; expires=" + date.toGMTString();
-                }
-                else {
-                    expires = "";
-                }
-                document.cookie = name + "=" + escape(value) + expires + "; path=/";
+            document.cookie = name + "=" + escape(value) + expires + "; path=/";
+        }
+    },
+    setShort: function (name, value, seconds) {
+        var expires;
+        if (name) {
+            if (seconds) {
+                var date = new Date();
+                date.setTime(date.getTime() + (seconds * 1000));
+                expires = "; expires=" + date.toGMTString();
             }
-        },
-        get: function (name, callback) {
-        	if(ApexChat.framed){
-                var id = (new Date()).getTime()
-          	    window.postMessage({
-                    id: id,
-            	    method: "ApexChat.Cookies.get",
-            	    arguments: [name]
-                });
-                ApexChat.Callbacks[id] = callback
-          	    return
+            else {
+                expires = "";
             }
-            if (name) {
-                var match = document.cookie.match(new RegExp(name + "=(.*?)(?:;|$)"));
-                if (match) {
-                    callback(unescape(match[1].replace(/\+/g, "%20")))
-                }else{
-                    callback()
-                }
+            document.cookie = name + "=" + escape(value) + expires + "; path=/";
+        }
+    },
+    get: function (name, callback) {
+        if(ApexChat.framed){
+            var id = (new Date()).getTime()
+            window.postMessage({
+                id: id,
+                method: "ApexChat.Cookies.get",
+                arguments: [name]
+            });
+            ApexChat.Callbacks[id] = callback
+            return
+        }
+        if (name) {
+            var match = document.cookie.match(new RegExp(name + "=(.*?)(?:;|$)"));
+            if (match) {
+                callback(unescape(match[1].replace(/\+/g, "%20")))
             }else{
                 callback()
             }
-        },
-
-        list: function (callback) {
-            if(ApexChat.framed){
-                var id = (new Date()).getTime()
-          	    window.postMessage({
-                    id: id,
-            	    method: "ApexChat.Cookies.list",
-            	    arguments: []
-                });
-                ApexChat.Callbacks[id] = callback
-          	    return
-            }
-            var pairs = document.cookie.split(new RegExp("; "));
-            if (pairs.length > 0 && pairs[0] !== "") {
-                var keys = new Array(pairs.length);
-                for (var i = 0; i < pairs.length; i++) {
-                    keys[i] = pairs[i].match(/(.+?)=/)[1];
-                }
-                callback(keys)
-            }
-            callback([]);
-        },
-
-        // delete is a reserved word, so appending an underscore
-        del: function (name) {
-            this.set(name, "", -1);
+        }else{
+            callback()
         }
-    };
+    },
 
-ApexChat.Cookies.set('test', 'this is a test');
-ApexChat.Cookies.get('test', function(result){
-    document.getElementById('out').innerHTML = result
-})
+    list: function (callback) {
+        if(ApexChat.framed){
+            var id = (new Date()).getTime()
+            window.postMessage({
+                id: id,
+                method: "ApexChat.Cookies.list",
+                arguments: []
+            });
+            ApexChat.Callbacks[id] = callback
+            return
+        }
+        var pairs = document.cookie.split(new RegExp("; "));
+        if (pairs.length > 0 && pairs[0] !== "") {
+            var keys = new Array(pairs.length);
+            for (var i = 0; i < pairs.length; i++) {
+                keys[i] = pairs[i].match(/(.+?)=/)[1];
+            }
+            callback(keys)
+        }
+        callback([]);
+    },
+
+    // delete is a reserved word, so appending an underscore
+    del: function (name) {
+        this.set(name, "", -1);
+    }
+};
